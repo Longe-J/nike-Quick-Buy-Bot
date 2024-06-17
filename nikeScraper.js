2f1b668813ea479af15aa64ee308f839740a2cb6
Longe-J <161849012+Longe-J@users.noreply.github.com>
2024-06-17 12:47:59 +0100

    updates

--- nikeScraper.js
+++ nikeScraper.js
@@ -1,13 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async ()=> {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://www.nike.com/gb/launch/t/air-jordan-1-low-silver-emea');
     await page.click('#size_item_radio12');
-    await page.evaluate(() =>{
-        
-    })
+   
     
     await page.click('.ncss-btn-primary-dark.btn-lg.buying-tools-cta-button');
 })();
